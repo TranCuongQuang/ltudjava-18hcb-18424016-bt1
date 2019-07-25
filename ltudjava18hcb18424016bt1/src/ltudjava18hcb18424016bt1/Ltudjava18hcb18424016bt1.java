4b0d85813900f755c20df8c4629159841c4f1553
Tran Quang Cuong <cuongtq@s-wifi.vn>
2019-07-25 23:57:18 +0700

    import file csv class list

--- ltudjava18hcb18424016bt1/src/ltudjava18hcb18424016bt1/Ltudjava18hcb18424016bt1.java
+++ ltudjava18hcb18424016bt1/src/ltudjava18hcb18424016bt1/Ltudjava18hcb18424016bt1.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package ltudjava18hcb18424016bt1;
 
 import java.io.BufferedReader;
@@ -13,64 +8,146 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-/**
- *
- * @author quang
- */
 public class Ltudjava18hcb18424016bt1 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+    public static void WriteData(String Path, String Data) {
+        try (FileWriter fw = new FileWriter(Path, true);
+                BufferedWriter bw = new BufferedWriter(fw);
+                PrintWriter out = new PrintWriter(bw)) {
+            out.println(Data);
+        } catch (IOException e) {
+        }
+    }
 
-        // Tạo một đối tượng BufferedReader
+    public static int ChooseFunction() throws IOException {
         BufferedReader dataIn = new BufferedReader(new InputStreamReader(System.in));
-        // Biến name
-
-        String name = "";
-        System.out.println("Please Enter Your Name:");
+        int function = 0;
+        System.out.println("Chọn chức năng: ");
+        System.out.println("1: Import danh sách sinh viên 1 lớp.");
+        System.out.println("2: Thêm thủ công sinh viên.");
+        System.out.println("3: Thêm, xóa thủ công sinh viên khỏi tkb lớp học.");
 
         // Tiến hành đọc từ bàn phím
-        try {
-            name = dataIn.readLine();
-        } catch (IOException e) {
-            System.out.println("Error!");
-        }
+        String strF = dataIn.readLine();
+//        Pattern pattern = Pattern.compile("\\d*");
+//        Matcher matcher = pattern.matcher(strF);
+//        if (matcher.matches()) {
+        function = Integer.parseInt(strF);
+        System.out.println("Bạn đã chọn chức năng: " + function);
+//            return function;
+//        } else {
+//            System.out.println("Bạn vừa nhập vào không phải số, vui lòng nhập lại !");
+//            ChooseFunction();
+//        }
+        return function;
 
-        // hiển thị tên
-        System.out.println("Hello " + name + "!");
+    }
 
-        String csvFile = "../../input/" + name;
-        BufferedReader br = null;
-        String line = "";
-        String cvsSplitBy = ",";
-        String Output = "database/" + name;
-        try {
-            PrintWriter pw = new PrintWriter(Output);
-            br = new BufferedReader(new FileReader(csvFile));
-            while ((line = br.readLine()) != null) {
-                //String[] country = line.split(cvsSplitBy);
-                //System.out.println("Country [code= " + country[4] + " , name=" + country[5] + "]");
-                System.out.println(line);
-                pw.close();
-                try (FileWriter fw = new FileWriter(Output, true);
-                        BufferedWriter bw = new BufferedWriter(fw);
-                        PrintWriter out = new PrintWriter(bw)) {
-                    out.println(line);
-                } catch (IOException e) {
+    public static int Countiue() throws IOException {
+        BufferedReader dataIn = new BufferedReader(new InputStreamReader(System.in));
+        int continueF = 0;
+        System.out.print("Thực hiện tiếp chương trình (1: Có, 0: Không): ");
+        String strC = dataIn.readLine();
+//        Pattern pattern = Pattern.compile("\\d*");
+//        Matcher matcher = pattern.matcher(strC);
+//        if (matcher.matches()) {
+        continueF = Integer.parseInt(strC);
+//        } else {
+//            Countiue();
+//        }
+        return continueF;
+    }
+
+    public static void DQ() throws IOException {
+        BufferedReader dataIn = new BufferedReader(new InputStreamReader(System.in));
+        int function = ChooseFunction();
+        if (function == 1) {
+            String input = "";
+            String output = "";
+            System.out.print("Nhập tên file inport: ");
+            input = dataIn.readLine();
+            System.out.print("Nhập tên file lưu: ");
+            output = dataIn.readLine();
+
+            // Đọc và ghi file
+            BufferedReader br = null;
+            String line = "";
+            String Path = "database/Class_" + output + ".txt";
+            try {
+                PrintWriter pw = new PrintWriter(Path);
+                br = new BufferedReader(new FileReader(input));
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                    pw.close();
+                    WriteData(Path, line);
+                }
+            } catch (FileNotFoundException e) {
+            } catch (IOException e) {
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                    }
+                } else {
+                    System.out.println("Lớp học không tồn tại.");
                 }
             }
-        } catch (FileNotFoundException e) {
-        } catch (IOException e) {
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
+        } else if (function == 2) {
+            String ClassName = "";
+            String StudentID = "";
+            String Name = "";
+            String Gender = "";
+            String CardNumber = "";
+
+            System.out.print("Nhập tên lớp: ");
+            ClassName = dataIn.readLine();
+            String Path = "database/Class_" + ClassName + ".txt";
+            System.out.print("Nhập MSSV: ");
+            StudentID = dataIn.readLine();
+            System.out.print("Nhập tên SV: ");
+            Name = dataIn.readLine();
+            System.out.print("Nhập giới tính: ");
+            Gender = dataIn.readLine();
+            System.out.print("Nhập CMND: ");
+            CardNumber = dataIn.readLine();
+
+            BufferedReader br = null;
+            String line = "";
+            try {
+                //PrintWriter pw = new PrintWriter(Path);
+                br = new BufferedReader(new FileReader(Path));
+                while ((line = br.readLine()) != null) {
+                    System.out.println(line);
+                    //pw.close();
+                    //WriteData(Path, line);
+                }
+            } catch (FileNotFoundException e) {
+            } catch (IOException e) {
+            } finally {
+                if (br != null) {
+                    try {
+                        br.close();
+                    } catch (IOException e) {
+                    }
+                } else {
+                    System.out.println("Lớp học không tồn tại.");
                 }
             }
+        } else {
+            System.out.print("Chức năng không tồn tại. Vui lòng kiểm tra lại.");
         }
+
+        int continueF = Countiue();
+        if (continueF == 1) {
+            DQ();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        DQ();
     }
 }
